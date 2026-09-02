0ddc7074d1f3c0d51583e88fa90b215e037b2d32
alexpax85 <pacealessio85@gmail.com>
2026-09-03 00:35:44 +0200

    Aggiorna al sorgente 19366f7

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
    senza che nessuno debba fare niente.
    ===================================================================== */
 
-const VERSIONE = 'portachiavi-824cb3e2';
+const VERSIONE = 'portachiavi-74b99dde';
 
 /* Cosa serve per partire. Il grosso e' tutto dentro index.html, che porta
    dentro di se' font, librerie e programma: un file solo, circa un mega. */
